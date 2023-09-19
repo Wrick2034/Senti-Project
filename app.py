bf4ee7a6803cbe50d1c731c1a4a31952f6482c65
Wrick2034 <souvikganguly125@gmail.com>
2023-09-19 14:39:42 +0530

    Create Backend for API

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask,render_template
-import pandas as pd
-import senti
+from flask import Flask
+from flask_restful import Api
+from application.controller import *
 
 # Configuration for the app
 app = Flask(__name__)
+api = Api(app)
 
-# Controller for index page
-@app.route('/',methods=['GET','POST'])
-def index():
-    df=pd.read_csv('Restaurant_Reviews.tsv',sep='\t')
-    df['sentiment'] = df['Review'].apply(senti.get_sentiment)
-    return render_template('index.html',df=df)
+# Add routes for API resources
+api.add_resource(Senti, '/api/sentiment/')
+
 
 if __name__ == "__main__":
     app.debug=True
